c67ceed72f459533069460189b2bc43a385eaaea
Reid Watson <j.watsonreid@gmail.com>
2020-09-01 16:03:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,9 +29,11 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while array[counter] do
-    array[counter].to_s
+    new_array.unshift(array[counter].to_s)
     counter += 1
   end
+  return new_array
 end
     
