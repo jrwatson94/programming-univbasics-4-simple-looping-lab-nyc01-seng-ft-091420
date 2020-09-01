60eaf078b65a87b826a6736ea1be3c9f4fb20919
Reid Watson <j.watsonreid@gmail.com>
2020-09-01 15:53:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,7 +27,7 @@ def output_array(array)
   end
 end
 
-def return_string_array(array)
-  counter = 0
-  while array[counter] do
+#def return_string_array(array)
+  #counter = 0
+  #while array[counter] do
     
